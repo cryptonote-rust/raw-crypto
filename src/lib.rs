8274dfbebf0d51c438c94496acb27a32f8f322b4
calidion <calidion@gmail.com>
2019-06-04 16:04:31 +0800

    enabled fast hash

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ const CHACHA_IV_SIZE: usize = 8;
 extern "C" {
   fn chacha8(data: *const u8, length: usize, key: *const u8, iv: *const u8, cipher: *mut u8);
   fn cn_slow_hash(data: *const u8, length: usize, hash: *mut u8, variant: usize, prehashed: usize);
+  fn cn_fast_hash(data: *const u8, length: usize, hash: *mut u8);
   fn secret_key_to_public_key(secret_key: *const u8, public_key: *mut u8) -> bool;
 }
 
@@ -78,6 +79,12 @@ pub fn secret_to_public(secret_key: &[u8; 32]) -> [u8; 32] {
   public_key
 }
 
+pub fn fast_hash(data: &[u8]) -> [u8; 32] {
+  let mut hash: [u8; 32] = [0; 32];
+  unsafe { cn_fast_hash(data.as_ptr(), data.len(), hash.as_mut_ptr()) }
+  hash
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -117,10 +124,59 @@ mod tests {
 
   #[test]
   fn should_get_public_key_from_secret_key() {
-    let secret_key = [50, 228, 229, 247, 39, 151, 194, 252, 14, 45, 218, 78, 128, 230, 27, 208, 9, 57, 52, 163, 5, 175, 8, 201, 211, 185, 66, 113, 88, 68, 170, 8];
+    let secret_key = [
+      50, 228, 229, 247, 39, 151, 194, 252, 14, 45, 218, 78, 128, 230, 27, 208, 9, 57, 52, 163, 5,
+      175, 8, 201, 211, 185, 66, 113, 88, 68, 170, 8,
+    ];
     let public_key = secret_to_public(&secret_key);
     println!("{:?}", public_key);
     println!("{:?}", public_key);
-    assert!(public_key == [81, 76, 248, 201, 237, 192, 109, 39, 58, 159, 67, 13, 120, 203, 91, 70, 36, 216, 162, 222, 0, 100, 243, 152, 32, 48, 89, 129, 252, 169, 180, 36]);
+    assert!(
+      public_key
+        == [
+          81, 76, 248, 201, 237, 192, 109, 39, 58, 159, 67, 13, 120, 203, 91, 70, 36, 216, 162,
+          222, 0, 100, 243, 152, 32, 48, 89, 129, 252, 169, 180, 36
+        ]
+    );
+  }
+
+  #[test]
+  fn should_get_fast_hash() {
+    let input = [
+      0x01, 0x3c, 0x01, 0xff, 0x00, 0x01, 0x01, 0x02, 0x9b, 0x2e, 0x4c, 0x02, 0x81, 0xc0, 0xb0,
+      0x2e, 0x7c, 0x53, 0x29, 0x1a, 0x94, 0xd1, 0xd0, 0xcb, 0xff, 0x88, 0x83, 0xf8, 0x02, 0x4f,
+      0x51, 0x42, 0xee, 0x49, 0x4f, 0xfb, 0xbd, 0x08, 0x80, 0x71, 0x21, 0x01, 0xa9, 0xa4, 0x56,
+      0x9f, 0x7e, 0x10, 0x16, 0x4a, 0x32, 0x32, 0x4b, 0x2b, 0x87, 0x8a, 0xe3, 0x2d, 0x98, 0xbe,
+      0x09, 0x49, 0xce, 0x6e, 0x01, 0x50, 0xba, 0x1d, 0x7e, 0x54, 0xd6, 0x09, 0x69, 0xe5,
+    ];
+    let hash = fast_hash(&input);
+    assert!(
+      hash
+        == [
+          81, 131, 30, 137, 17, 68, 149, 122, 23, 4, 105, 195, 35, 123, 221, 255, 230, 192, 96, 73,
+          129, 38, 117, 210, 237, 178, 168, 52, 82, 247, 162, 80
+        ]
+    );
+    let input = [
+      50, 228, 229, 247, 39, 151, 194, 252, 14, 45, 218, 78, 128, 230, 27, 208, 9, 57, 52, 163, 5,
+      175, 8, 201, 211, 185, 66, 113, 88, 68, 170, 8,
+    ];
+    let hash = fast_hash(&input);
+    assert!(
+      hash
+        == [
+          197, 173, 69, 56, 117, 95, 205, 188, 226, 116, 198, 205, 121, 235, 248, 74, 211, 46, 177,
+          65, 72, 147, 115, 194, 212, 214, 163, 109, 121, 3, 36, 249
+        ]
+    );
+    let input = [50];
+    let hash = fast_hash(&input);
+    assert!(
+      hash
+        == [
+          173, 124, 91, 239, 2, 120, 22, 168, 0, 218, 23, 54, 68, 79, 181, 138, 128, 126, 244, 201,
+          96, 59, 120, 72, 103, 63, 126, 58, 104, 235, 20, 165
+        ]
+    );
   }
 }
